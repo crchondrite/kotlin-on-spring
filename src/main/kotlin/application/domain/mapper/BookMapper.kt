28f8df8f296f234ff9afff3206f3e77e854f1922
Hiroyuki Iio <crchondrite@gmail.com>
2018-02-04 18:57:15 +0900

    add comment

--- src/main/kotlin/application/domain/mapper/BookMapper.kt
+++ src/main/kotlin/application/domain/mapper/BookMapper.kt
@@ -4,6 +4,8 @@ import application.domain.Book
 import org.apache.ibatis.annotations.Mapper
 import org.apache.ibatis.annotations.Select
 
+// multi datasource で hogehogeするにはもうちょっと工夫が必要かも
+// https://qiita.com/KevinFQ/items/a8de808e7719315c376c
 @Mapper
 interface BookMapper {
     @Select("""
